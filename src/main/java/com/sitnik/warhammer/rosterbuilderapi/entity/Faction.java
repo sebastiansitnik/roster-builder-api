b88fa9a603db8e6a3a4bd4103d308a6c990f2904
Seba <2sitto@gmail.com>
2026-03-26 13:29:09 +0100

    fanction added

--- src/main/java/com/sitnik/warhammer/rosterbuilderapi/entity/Faction.java
+++ src/main/java/com/sitnik/warhammer/rosterbuilderapi/entity/Faction.java
@@ -1,6 +1,21 @@
 package com.sitnik.warhammer.rosterbuilderapi.entity;
 
-@Entity
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Table(name = "factions")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Faction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(unique = true, nullable = false)
+    private String name;
 }
