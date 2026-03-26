b7184cec99402edcd15082e1d5d98f27db1e74ef
Seba <2sitto@gmail.com>
2026-03-26 11:45:01 +0100

    Initial commit

---
+++ src/main/java/pl/sitnik/warhammer/rosterbuilderapi/RosterBuilderApiApplication.java
@@ -0,0 +1,13 @@
+package pl.sitnik.warhammer.rosterbuilderapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RosterBuilderApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RosterBuilderApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/sitnik/warhammer/rosterbuilderapi/RosterBuilderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sitnik.warhammer.rosterbuilderapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RosterBuilderApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
